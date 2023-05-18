228d15805f872158d0d60a456c20afecc2b0b1a6
unknown <Miragha-Aliyev>
2023-05-18 15:29:06 +0400

    8 ci ev tapshiriqi

---
+++ main.js
@@ -0,0 +1,110 @@
+// ====================================
+// ===========Homework 1===============
+// ====================================
+
+
+// let text = "tapshiriq 1"
+
+// console.log(text)
+
+// let newText = text.toUpperCase ()
+
+// console.log (newText)
+
+
+
+// ====================================
+// ===========Homework 2===============
+// ====================================
+
+// let name = "The quote \"There is no exercise better for the heart than reaching down and lifting people up.\" by John Holmes teaches us to help one another."
+
+// console.log("name")
+
+
+
+// ====================================
+// ===========Homework 3===============
+// ====================================
+
+// let userRegst = prompt ("username")
+// let passResgt = prompt ("password")
+
+// userRegst = userRegst.toUppercase();
+// userRegst =userRegst.toLowercase()
+
+// console.log(userregst)
+// console.log(passresgt)
+
+// let user = {
+//     username: userRegst,
+//     password: passResgt
+// }
+
+
+
+// let userLogin = prompt ("username1")
+// let userPass = prompt ("password1")
+
+// if (userRegst == userLogin && passResgt == userPass){
+
+//     console.log ("xosh geldun")
+// }
+
+
+// else if (userLogin!=user.username){
+
+//     console.log("dogru ad daxil olunmayib")
+// }
+
+
+// else if (userPass!=user.password){
+
+//     console.log("dogru parol daxil olunmayib")
+// }
+
+
+// else{
+
+//     console.log("yanlish girish")
+
+// }
+
+
+
+
+// console.log(userLogin)
+// console.log(userPass)
+
+
+
+// ====================================
+// ===========Homework 4===============
+// ====================================
+
+// let numbers = "1\n2 2\n3 3 3\n4 4 4 4\n5 5 5 5 5\n6 6 6 6 6 6"
+
+// console.log (numbers)
+
+
+
+// ====================================
+// ===========Homework 5===============
+// ====================================
+
+
+let texts = "There is no exercise better for the heart than reaching down and lifting people up.";
+
+let wordCounts = 0;
+    let counts = i
+
+function wordCounts(){
+
+
+    for (let i=0; i<texts.length; i++);
+
+    let  = wordCounts [i]
+
+
+}
+
